0ddcd17f9a73473638b8f8c3af6052759d260aea
Bapi Majumder <122961065+BapiMajumder1402@users.noreply.github.com>
2023-05-16 13:02:46 +0530

    Task adding done

--- src/Component/CardComponent/CardComponent.js
+++ src/Component/CardComponent/CardComponent.js
@@ -1,43 +1,31 @@
-import React, { useState } from 'react'
-import PopOver from '../PopOver/PopOver'
-import card from './card.module.css'
-import { Input } from '@chakra-ui/react'
-import { Button } from '@chakra-ui/react'
-import { useRecoilState } from 'recoil'
-import { TodoAtom, ListAtom } from '../Atom/Atom'
-import { AiOutlineCloseCircle } from 'react-icons/ai'
-
+import React, { useState } from 'react';
+import PopOver from '../PopOver/PopOver';
+import card from './card.module.css';
+import { Input } from '@chakra-ui/react';
+import { Button } from '@chakra-ui/react';
+import { useRecoilState } from 'recoil';
+import { TodoAtom, ListAtom } from '../Atom/Atom';
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 function CardComponent({ id, tasklist }) {
-    const [mainList, setMainList] = useRecoilState(ListAtom)
-    const [show, setshow] = useState(false)
+    const [mainList, setMainList] = useRecoilState(ListAtom);
+    const [show, setshow] = useState(false);
+    const [del , setDel]= useState(false)
     const [todo, setTodo] = useState({
-        id: Date.now() + Math.random() * 500,
-        task: "",
-    })
-    const [todoList, setTodoList] = useRecoilState(TodoAtom)
-    function toogle() {
-        setshow(!show)
-    }
-    function changeHandler(e) {
-        const { name, value } = e.target
-        setTodo({ ...todo, [name]: value })
-        console.log(todo);
-    }
-    // function submitTodo(){
-    //     setTodoList([...todoList,todo])
-    //     localStorage.setItem("todo", JSON.stringify([...todoList,todo]));
-    //     setTodo({
-    //         id:Date.now()+Math.random()*500,
-    //         task:""
-    //     })
-    //     let list1 = [...mainList]
-    //     let currentList= list1.filter(elm=>elm.listId===id)[0]
-    //     console.log(currentList);
-    //     currentList.task =[...currentList.task,todo]
-    //     console.log(currentList);
+        id: Date.now() + Math.random() * 1000,
+        task: '',
+        date: new Date(),
+    });
 
-    // }
+    function toogle() {
+        setshow(!show);
+    }
+
+    function changeHandler(e) {
+        const { name, value } = e.target;
+        setTodo({ ...todo, [name]: value });
+    }
+
     function submitTodo() {
         let list1 = [...mainList];
         let currentListIndex = list1.findIndex((elm) => elm.listId === id);
@@ -46,36 +34,52 @@ function CardComponent({ id, tasklist }) {
             currentList.tasklist = [...currentList.tasklist, todo];
             list1[currentListIndex] = currentList;
             setMainList(list1);
-            localStorage.setItem('List', JSON.stringify(mainList))
+            localStorage.setItem('List', JSON.stringify(mainList));
         }
+        setTodo({
+            id: Date.now() + Math.random() * 500,
+            task: '',
+            date: new Date(),
+        });
     }
-    console.log(todoList)
+
     return (
         <div className={card.container}>
-            <div>{mainList.map((item) => {
-                return item.tasklist.map((elm) => {
-                    return (
-                        <div className={card.todo} key={elm.id}>
-                            <p>{elm.task}</p>
-                            <p><PopOver /></p>
-                        </div>
-                    );
-                });
-            })}</div>
-
-            <div className={card.card}>
-                {!show ? <p className={card.addBtn} onClick={toogle}>+ Add a Card</p> :
-                    <div>
-                        <Input name='task' value={todo.task} placeholder='Add task Here' onChange={changeHandler} />
-                        <div className={card.btns}>
-                            <Button colorScheme='blue' onClick={submitTodo}>Add</Button>
-                            <AiOutlineCloseCircle onClick={toogle} />
-                        </div>
-                    </div>}
+            <div>
+                {tasklist.map((elm) => (
+                    <div className={card.todo} key={elm.id}>
+                        <p>{elm.task}</p>
+                        <p>
+                            <PopOver />
+                        </p>
+                    </div>
+                ))}
             </div>
 
+            <div className={card.card}>
+                {!show ? (
+                    <p className={card.addBtn} onClick={toogle}>
+                        + Add a Card
+                    </p>
+                ) : (
+                    <div>
+                        <Input
+                            name="task"
+                            value={todo.task}
+                            placeholder="Add task Here"
+                            onChange={changeHandler}
+                        />
+                        <div className={card.btns}>
+                            <Button colorScheme="blue" onClick={submitTodo}>
+                                Add
+                            </Button>
+                            <AiOutlineCloseCircle onClick={toogle} />
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-    )
+    );
 }
 
-export default CardComponent
+export default CardComponent;
